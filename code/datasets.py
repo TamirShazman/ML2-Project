cf9a48c5e3eb7e01be59e0e13ce8ee3030cadc46
tshatzman <tamirshazman@gmail.com>
2022-02-14 15:52:11 +0200

    initial commit

---
+++ code/datasets.py
@@ -0,0 +1,30 @@
+from torch.utils.data import Dataset
+import torchvision
+
+
+class MNIST(torchvision.datasets.MNIST):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, index):
+        return super().__getitem__(index)[0]
+
+
+class CIFAR10(torchvision.datasets.CIFAR10):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, index):
+        return super().__getitem__(index)[0]
+
+
+class CELEBA(torchvision.datasets.ImageFolder):
+    def __init__(self, root='./data/celeba/', train=True, transform=None):
+        if train:
+            root = root + 'train'
+        else:
+            root = root + 'test'
+        super().__init__(root=root, transform=transform)
+
+    def __getitem__(self, index):
+        return super().__getitem__(index)[0]
